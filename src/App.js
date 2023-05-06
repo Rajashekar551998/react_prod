4067db4bb5f6a5a34b06dce81fa6af7a517c48a6
Venkata Rajashekar Reddy <rajashekar.chintala55@gmail.com>
2023-05-06 14:39:17 -0500

    Added Button

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <div>Hii</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <button type="button" class="btn">
+              Base class
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
