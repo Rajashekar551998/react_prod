b827620c9df18b90668a744e96129152fc0b9a4f
Venkata Rajashekar Reddy <rajashekar.chintala55@gmail.com>
2023-07-30 19:25:49 -0500

    Added changes

--- src/Recentwork.jsx
+++ src/Recentwork.jsx
@@ -18,13 +18,20 @@ function Recentwork() {
           </a>
         </p>
         <div className="container d-flex justify-content-center mt-5">
-          <div className="card" style={{ width: "25rem", padding: "20px" }}>
+          <div
+            className="card"
+            style={{
+              width: "25rem",
+              padding: "17px",
+              border: "1px solid #6e07f3",
+            }}
+          >
             <div className="card-body">
               <h5 className="card-title">Akria Technologies LLC</h5>
               <img
                 src={akrialogo}
                 alt="AkriaLogo"
-                style={{ width: "80%", height: "80%", pointerEvents: "none" }}
+                style={{ width: "80%", height: "65%", pointerEvents: "none" }}
               />
             </div>
             <div className="text-center">
@@ -34,6 +41,20 @@ function Recentwork() {
             </div>
           </div>
         </div>
+      </div>
+      <div className="container" style={{ margin: "40px" }}>
+        <h4 style={{ fontSize: "28px" }}>Interested to Hire me:</h4>
+        <p style={{ fontSize: "20px" }} className="mt-3">
+          <i
+            class="bi bi-shuffle"
+            style={{ marginRight: "5px", color: "#6e07f3" }}
+          ></i>
+          I’m always open to discussing product design & Development work.
+        </p>
+        <button className="start_con mt-3">
+          <i class="bi bi-chat-left-dots" style={{ marginRight: "5px" }}></i>
+          Start Conversation
+        </button>
       </div>
     </React.Fragment>
   );
