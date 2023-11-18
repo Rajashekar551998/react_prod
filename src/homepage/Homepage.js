c1f05b1abb5c4c0a79694a619c028a8f0045ee02
Venkata Rajashekar Reddy <rajashekar.chintala55@gmail.com>
2023-11-18 14:39:54 -0700

    Added new Code

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Modal, Button } from "react-bootstrap";
 import "./Homepage.css";
 import avatar from "../Images/avatar.png";
 import devices from "../Images/devices.svg";
 import Intro from "../Intro";
 import Recentwork from "../Recentwork";
+import merryChristimas from "../Images/christimas.webp";
+import thanksGiving from "../Images/thanksGiving.jpg";
 
 function Homepage() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPopup(true);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClose = () => setShowPopup(false);
   return (
     <React.Fragment>
+      <Modal show={showPopup} onHide={handleClose}>
+        <Modal.Body>
+          <div className="text-center">
+            <img src={thanksGiving} style={{ width: "450px" }} />
+          </div>
+          <div className="text-center mt-3">
+            <img
+              src={merryChristimas}
+              style={{ width: "450px", height: "300px" }}
+            />
+          </div>
+        </Modal.Body>
+      </Modal>
       <div className="container text-center full_page_text">
         <h2
           style={{
